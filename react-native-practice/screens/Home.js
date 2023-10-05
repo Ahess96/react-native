6a50acf5b629ed1d292c4e816b45fc353ca48880
Austin Hess <austinhess1996@comcast.net>
2023-10-05 13:30:36 -0700

    Create Home component

---
+++ react-native-practice/screens/Home.js
@@ -0,0 +1,23 @@
+import { StyleSheet, Text, SafeAreaView } from "react-native";
+import React from "react";
+
+const Home = () => {
+    return (
+        <SafeAreaView style={styles.container}>
+            <Text style={styles.mainText}>Home Comp</Text>
+        </SafeAreaView>
+    )
+
+    const styles = StyleSheet.create({
+        container: {
+            flex: 1,
+            justifyContent: 'space-between'
+        },
+        mainText: {
+            fontSize: 30,
+            textAlign: 'center'
+        }
+    })
+}
+
+export default Home;
