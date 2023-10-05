d3a1a16505b7bfab66e54b50a9016e2108b77169
Ahess96 <austinhess1996@comcast.net>
2023-10-04 19:02:06 -0700

    implement login functionality

--- react-native-practice/App.js
+++ react-native-practice/App.js
@@ -1,10 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
 import SignUp from './screens/SignUp';
+import SignIn from './screens/SignIn';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <SignUp />
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='SignIn'>
+        <Stack.Screen name='SignUp' component={SignUp} />
+        <Stack.Screen name='SignIn' component={SignIn} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
--- react-native-practice/screens/SignUp.js
+++ react-native-practice/screens/SignUp.js
@@ -1,21 +1,41 @@
-import {StyleSheet, Text, View, TextInput} from 'react-native';
+import {StyleSheet, Text, View, TextInput, TouchableOpacity, Image} from 'react-native';
 import React, {useState} from 'react';
+import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
 
-const SignUp = () => {
+const SignUp = ({navigation}) => {
 
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
+    const handleSubmit = async () => {
+        if (name === '' || email === '' || password === '') {
+            alert('All fields are required');
+            return;
+        }
+        await axios.post('http:localhost:8001/api/signup', {
+            name,
+            email,
+            password
+        });
+        alert('Sign up successful');
+    };
+
     return (
-        <View style={styles.container}>
+      <KeyboardAwareScrollView contentContainerStyle={styles.container}>
+        <View style={{marginVertical: 100}}>
+            <View style={styles.imageContainer}>
+                <Image source={require('../assets/favicon.png')} style={styles.imageStyles} />
+            </View>
             <Text style={styles.signupText}>SignUp</Text>
             <View style={{ marginHorizontal: 24 }}>
                 <Text style={{ fontSize: 16, color: '#8e93a1' }}>NAME</Text>
                 <TextInput 
                     style={styles.signupInput}
                     value={name}
-                    onChangeText={text => setName(text)} 
+                    onChangeText={text => setName(text)}
+                    autoCapitalize='words' 
+                    autoCorrect={false}
                 />
             </View>
             <View style={{marginHorizontal: 24}}>
@@ -24,6 +44,8 @@ const SignUp = () => {
                     style={styles.signupInput}
                     value={email}
                     onChangeText={text => setEmail(text)}
+                    autoCompleteType='email'
+                    keyboardType='email-address'
                 />
             </View>
             <View style={{marginHorizontal: 24}}>
@@ -32,10 +54,26 @@ const SignUp = () => {
                     style={styles.signupInput}
                     value={password}
                     onChangeText={text => setPassword(text)}
+                    secureTextEntry={true}
+                    autoCompleteType='password'
                 />
             </View>
-            <Text style={{marginHorizontal: 24}}>{JSON.stringify({name, email, password})}</Text>
+            <TouchableOpacity 
+                onPress={handleSubmit}
+                style={styles.buttonStyle}>
+                    <Text style={styles.buttonText}>Submit</Text>
+            </TouchableOpacity>
+            <Text style={{fontSize: 12, textAlign: 'center'}}>
+                Already Joined? {' '}
+                <Text
+                    style={{color: 'darkred', fontWeight: 'bold'}}
+                    onPress={() => navigation.navigate('SignIn')}
+                >
+                    Sign In
+                </Text>
+            </Text>
         </View>
+      </KeyboardAwareScrollView>
     )
 }
 
@@ -53,6 +91,30 @@ const styles = StyleSheet.create({
         height: 48,
         borderBottomColor: '#8e93a1',
         marginBottom: 30
+    },
+    buttonStyle: {
+        backgroundColor: 'darkmagenta',
+        height: 50,
+        marginBottom: 20,
+        justifyContent: 'center',
+        marginHorizontal: 15,
+        borderRadius: 15
+    },
+    buttonText: {
+        fontSize: 20, 
+        textAlign: 'center',
+        color: '#fff',
+        textTransform: 'uppercase',
+        fontWeight: 'bold'
+    },
+    imageContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    imageStyles: {
+        width: 100,
+        height: 100,
+        marginVertical: 20
     }
 })
 
